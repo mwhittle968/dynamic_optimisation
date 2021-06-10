815220e70472ed9baea832e67c1480570b7cefc9
mwhittle968 <mathildawhittle@gmail.com>
2021-06-10 15:06:39 +0100

    10th June

--- Model_3/functions_3.R
+++ Model_3/functions_3.R
@@ -7,23 +7,23 @@ chop <- function(x, x.min, x.max){
 
 # Extrinsic survival and state-dependent survival function based on maternal reserves
 
-S <- function(nh, nh.crit, t){
-  ex.survival = female$ex.survival[t] # Approx 36 time steps so 1/36 chance of dying
-  in.survival = ifelse(nh < nh.crit, 0, 1)
+S <- function(nh, t){
+  ex.survival = F$ex.surv[t]
+  in.survival = ifelse(nh < F$nh.crit[t], 0, 1)
   S = ex.survival*in.survival
   return(S)
 }
 
 # Fitness function based on maternal reserves and event step:
 
-B <- function(nh, ns, nh.repro, ns.repro, t, m){
-  if (female$reproduction == "no" | nh < nh.repro | ns < ns.repro){
+B <- function(nh, ns, s, t){
+  if (F$repro[t] == "no" | nh < nh.repro | ns < ns.repro | s < s.repro){
       B = 0
       } else {# If it's a reproductive time step  and the reserves are sufficient the fitness is calculated
-    nh.larva = (nh-female$mh[t])*0.6 # Mother gives half her reserves to larva
-    ns.larva = (ns-female$ms[t])*0.6
-    B.nh = (m*nh.larva)-m*(nh.repro-female$mh[t])*0.6 # linear by nh
-    B.ns = ifelse(ns < (ns.repro-female$ms[t])*0.6, 0, 1) # step function by ns
+    nh.l = (nh-F$mh[t])*nh.larva # Mother gives some of her reserves to larva
+    ns.l = (ns-F$ms[t])*ns.larva
+    B.nh = (m*nh.l)-m*(nh.repro-F$mh[t])*nh.larva # linear by nh
+    B.ns = ifelse(ns < (ns.repro-F$ms[t])*ns.larva, 0, 1) # step function by ns
     B = B.nh*B.ns
     }
   return(B)
@@ -31,41 +31,41 @@ B <- function(nh, ns, nh.repro, ns.repro, t, m){
 
 # Maintenance, investment, regulation and production
 
-maintenance <- function(s, i){
+maintenance <- function(s){
   m = s*i
   return(m)
 }
 
-investment <- function(s, j, d){
+investment <- function(s, d){
   s. = s + (d-1)*j
   return(s.)
 }
 
-regulation <- function(s, N, k, d){
-  s. = s - (d-N-1)*k
+regulation <- function(s, d){
+  s. = s - (d-n-1)*k
   return(s.)
 }
 
-production <- function(s, l){
+production <- function(s){
   p = s*l
   return(p)
 }
 
 # New state variables:
 
-new.state <- function(nh, ns, s, nh.max, ns.max, s.max, nh.repro, ns.repro, t, N, i, j, k, l, d){
-  nh.larva = ifelse(female$reproduction == "no" | nh < nh.repro | ns < ns.repro, 0, (nh - female$mh[t])*0.6)
-  ns.larva = ifelse(female$reproduction == "no" | nh < nh.repro | ns < ns.repro, 0, (ns - female$ms[t])*0.6)
-  new.ns = chop((ns - female$ms[t] - ns.larva + production(s, l)), 0, ns.max)
+new.state <- function(nh, ns, s, t, d){
+  nh.l = ifelse(F$repro[t] == "no" | nh < nh.repro | ns < ns.repro, 0, (nh - F$mh[t])*nh.larva)
+  ns.l = ifelse(F$repro[t] == "no" | nh < nh.repro | ns < ns.repro, 0, (ns - F$ms[t])*ns.larva)
+  new.ns = chop((ns - F$ms[t] - ns.larva + production(s)), 0, ns.max)
     if (d == 1){ # If d = 1, there is no investment or regulation
-      new.nh = chop((nh - nh.larva - female$mh[t] + female$n[t] - maintenance(s, i)), 0, nh.max)
+      new.nh = chop((nh - nh.l - F$mh[t] + F$N[t] - maintenance(s)), 0, nh.max)
       new.s = s
-      } else if (d > 1 & d <= (N+1)){ # If d is between 2 and N+1, there is investment
-      new.nh = chop((nh - female$mh[t] - nh.larva + female$n[t] - maintenance(s, i) - (d-1)), 0, nh.max)
-      new.s = chop((investment(s, j, d)), 0, s.max)
+      } else if (d > 1 & d <= (n+1)){ # If d is between 2 and N+1, there is investment
+      new.nh = chop((nh - F$mh[t] - nh.l + F$N[t] - maintenance(s) - (d-1)), 0, nh.max)
+      new.s = chop((investment(s, d)), 0, s.max)
       } else { # If d is greater than N+1, there is regulation
-      new.nh = chop((nh - female$mh[t] - nh.larva + female$n[t] - maintenance(s, i) - (d-N-1)), 0 , nh.max)
-      new.s = chop((regulation(s, N, k, d)), 0, s.max)
+      new.nh = chop((nh - F$mh[t] - nh.l + F$N[t] - maintenance(s) - (d-n-1)), 0 , nh.max)
+      new.s = chop((regulation(s, d)), 0, s.max)
       }
   output = data.frame(new.nh, new.ns, new.s)
   return(output)
