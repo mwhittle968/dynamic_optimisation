2f2d567fa9584471112c2825640c6b342c470be7
mwhittle968 <mathildawhittle@gmail.com>
2021-06-11 16:07:06 +0100

    11th June

---
+++ Model_3/Grid_search_3.R
@@ -0,0 +1,27 @@
+######################## Grid search ##########################
+
+# Set working directory
+setwd("C:/Users/mw14794/OneDrive - University of Bristol/Documents/PhD/Notes/Model/R_code")
+# setwd(working directory for laptop)
+
+# Packages
+library(tidyverse)
+library(reshape2)
+library(utils)
+
+# Functions
+source("Model_3/Model_3.R")
+
+I <- c(0.01, 0.05, 0.1, 0.5, 1)
+J <- c(1, 2, 6)
+K <- c(1, 2, 6)
+L <- c(0.2, 1, 1.5, 3)
+
+for (a in 1:5){
+  for (b in 1:3){
+    for (c in 1:3){
+      print(paste("Model.3.", a, b, c, sep = ""))
+      dynamic.optimisation.3(paste("3", a, b, c, sep = ""), i = I[a], j = J[b], k = K[b], l = L[c])
+    }
+  }
+}
--- Model_3/Model_3.R
+++ Model_3/Model_3.R
@@ -1,20 +1,13 @@
-# Dynamic optimisation model with look-ahead policy
-
-# Packages
-library(tidyverse)
-library(reshape2)
-library(utils)
-
-# Functions
-source("Model_3/functions_3.R")
-source("interpolate.R")
-source("Model_3/test_parameters.R")
+# Dynamic optimisation model with dynamic programming
 
 ###################################################################################################################################################
 
-dyanamic.optimisation <- function(model.name, nh.max, ns.max, s.max, nh.repro, ns.repro, s.repro, nh.crit.pupae, nh.crit.adult, nh.larva, ns.larva,
-                                  mh.pupae, mh.adult, ms.pupae, ms.adult, ex.surv.pupae, ex.surv.adult, T, n, m, i, j, k, l, Q){
-  
+dynamic.optimisation.3 <- function(model.name, nh.max = 20, ns.max = 5, s.max = 20, nh.repro = 8, ns.repro = 3, s.repro = 7, nh.crit.pupae = 4, nh.crit.adult = 4,
+                                   nh.larva = 0.6, ns.larva = 0.6, mh.pupae = 1, mh.adult = 2, ms.pupae = 2, ms.adult = 2, ex.surv.pupae = 1, ex.surv.adult = (1 - 1/61),
+                                   T = 61, n = 5, m = 1, i, j, k, l, Q = 100){
+source("interpolate.R")
+#ptm <- proc.time()
+
 ###################################################### Data frames for events and parameters ######################################################
 
 parameters <- c(model.name, nh.max, ns.max, s.max, nh.repro, ns.repro, s.repro, nh.crit.pupae, nh.crit.adult, nh.larva, ns.larva,
@@ -22,7 +15,7 @@ parameters <- c(model.name, nh.max, ns.max, s.max, nh.repro, ns.repro, s.repro, 
 parameters <- t(as.data.frame(parameters))
 colnames(parameters) <- c("model.name", "nh.max", "ns.max", "s.max", "nh.repro", "ns.repro", "s.repro", "nh.crit.pupae", "nh.crit.adult", "nh.larva", "ns.larva",
                      "mh.pupae", "mh.adult", "ms.pupae", "ms.adult", "ex.surv.pupae", "ex.surv.adult", "T", "n", "m", "i", "j", "k", "l", "Q")
-save(parameters, file = paste("Model_3/parameters_", model.name, ".RData", sep = ""))
+#save(parameters, file = paste("Model_3/parameters.", model.name, ".RData", sep = ""))
 
 time.step <- c(1:61)
 F <- as.data.frame(time.step)%>%
@@ -33,10 +26,80 @@ F <- as.data.frame(time.step)%>%
   mutate(ex.surv = ifelse(stage == "pupae", ex.surv.pupae, ex.surv.adult))%>%
   mutate(nh.crit = ifelse(stage == "pupae", nh.crit.pupae, nh.crit.adult))
 
+###################################################### Functions ######################################################
+
+# Chop function limits the range of values the state variables can take:
+chop <- function(x, x.min, x.max){
+  x = ifelse(x >= x.max, x.max, ifelse(x <= x.min, x.min, x))
+  return(x)
+}
+
+# Extrinsic survival and state-dependent survival function based on maternal reserves
+S <- function(nh, t){
+  ex.survival = F$ex.surv[t]
+  in.survival = ifelse(nh < F$nh.crit[t], 0, 1)
+  S = ex.survival*in.survival
+  return(S)
+}
+
+# Fitness function based on maternal reserves and event step:
+B <- function(nh, ns, s, t){
+  if (F$repro[t] == "no" | nh < nh.repro | ns < ns.repro | s < s.repro){
+    B = 0
+  } else {# If it's a reproductive time step  and the reserves are sufficient the fitness is calculated
+    nh.l = (nh-F$mh[t])*nh.larva # Mother gives some of her reserves to larva
+    ns.l = (ns-F$ms[t])*ns.larva
+    B.nh = (m*nh.l)-m*(nh.repro-F$mh[t])*nh.larva # linear by nh
+    B.ns = ifelse(ns < (ns.repro-F$ms[t])*ns.larva, 0, 1) # step function by ns
+    B = B.nh*B.ns
+  }
+  return(B)
+}
+
+# Maintenance, investment, regulation and production
+maintenance <- function(s){
+  m = s*i
+  return(m)
+}
+
+investment <- function(s, d){
+  s. = s + (d-1)*j
+  return(s.)
+}
+
+regulation <- function(s, d){
+  s. = s - (d-n-1)*k
+  return(s.)
+}
+
+production <- function(s){
+  p = s*l
+  return(p)
+}
+
+# New state variables:
+new.state <- function(nh, ns, s, t, d){
+  nh.l = ifelse(F$repro[t] == "no" | nh < nh.repro | ns < ns.repro, 0, (nh - F$mh[t])*nh.larva)
+  ns.l = ifelse(F$repro[t] == "no" | nh < nh.repro | ns < ns.repro, 0, (ns - F$ms[t])*ns.larva)
+  new.ns = chop((ns - F$ms[t] - ns.larva + production(s)), 0, ns.max)
+  if (d == 1){ # If d = 1, there is no investment or regulation
+    new.nh = chop((nh - nh.l - F$mh[t] + F$N[t] - maintenance(s)), 0, nh.max)
+    new.s = s
+  } else if (d > 1 & d <= (n+1)){ # If d is between 2 and N+1, there is investment
+    new.nh = chop((nh - F$mh[t] - nh.l + F$N[t] - maintenance(s) - (d-1)), 0, nh.max)
+    new.s = chop((investment(s, d)), 0, s.max)
+  } else { # If d is greater than N+1, there is regulation
+    new.nh = chop((nh - F$mh[t] - nh.l + F$N[t] - maintenance(s) - (d-n-1)), 0 , nh.max)
+    new.s = chop((regulation(s, d)), 0, s.max)
+  }
+  output = data.frame(new.nh, new.ns, new.s)
+  return(output)
+}
+
 ######################################################## Dynamic Programming with for loops #######################################################
 
 pb <- txtProgressBar(min = 0, max = ns.max*nh.max*s.max*(T-1), style = 3)
-V <- array(data = NA, dim = c((T), nh.max, ns.max, s.max)) # Empty fitness array, to be populated with values from T back to 1
+V <- array(data = NA, dim = c(T, nh.max, ns.max, s.max)) # Empty fitness array, to be populated with values from T back to 1
 d.opt <- array(data = NA, dim = c((T-1), nh.max, ns.max, s.max)) # Empty array for best decisions at each state and time
 V[T,,,] <- 0 # terminal fitness function
 for (t in (T-1):1){ # Iterates backwards in time
@@ -57,7 +120,9 @@ for (t in (T-1):1){ # Iterates backwards in time
     }
   }
 }
-save(d.opt, file = paste("Model_3/d.opt_", model.name, ".RData", sep = ""))
+assign(paste("d.opt.", model.name, sep = ""), d.opt)
+get(paste("d.opt.", model.name, sep = "")) %>%
+save(file = paste("Model_3/d.opt.", model.name, ".RData", sep = ""))
 
 ############################################################# Optimisation array plot ############################################################
 
@@ -69,7 +134,9 @@ d.opt. <- melt(d.opt)
     scale_fill_gradient(low = "#80CBC4", high = "#00695C")+
     theme(axis.title.y.right = element_text("Nh"),
           axis.title.x.top = element_text("Ns"))
-save(d.opt.plot, file = paste("Model_3/d.opt.plot_", model.name, ".RData", sep = ""))
+assign(paste("d.opt.plot.", model.name, sep = ""), d.opt.plot)
+get(paste("d.opt.plot.", model.name, sep = "")) %>%
+save(file = paste("Model_3/d.opt.plot.", model.name, ".RData", sep = ""))
 
 ################################################################### Simulations ##################################################################
 
@@ -88,18 +155,22 @@ for (t in 1:(T-1)){
     ns. = as.numeric(new.state(sim$nh.reserves[t], sim$ns.reserves[t], sim$sym.pop[t], t, d)[2])
     s. = as.numeric(new.state(sim$nh.reserves[t], sim$ns.reserves[t], sim$sym.pop[t], t, d)[3])
     sim <- add_row(sim, time.step = (t+1), nh.reserves = nh., ns.reserves = ns., sym.pop = s., decision = NA) # populate data frame with new state values
-    print(t)}
+    }
   else {
     break # if reserves get to zero, stop the simulation
   }
 }
-print(sim)
 sim.plot <- sim.plot+
 geom_line(data = sim, aes(x = time.step, y = sym.pop, group = 1)) # add data frame to plot
 }
 sim.plot <- sim.plot+
   labs(x = "time step", y = "symbiont density")
-save(sim.plot, file = paste("Model_3/sim.plot_", model.name, ".RData", sep = ""))
+assign(paste("sim.plot.", model.name, sep = ""), sim.plot)
+get(paste("sim.plot.", model.name, sep = "")) %>%
+save(file = paste("Model_3/sim.plot.", model.name, ".RData", sep = ""))
 
 ################################################################################################################################################
+
+#print(proc.time() - ptm)
 }
+
